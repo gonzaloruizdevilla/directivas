394de3556d2dcbcc0c45c50e2397baa72b9aa216
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2014-02-07 19:14:22 +0100

    (reloj): formatting

--- app/scripts/directives/reloj3.js
+++ app/scripts/directives/reloj3.js
@@ -2,16 +2,16 @@
 
 angular.module('directivasApp')
   .directive('reloj3', function () {
-  return {
-    templateUrl: 'templates/reloj3.html',
-    restrict: 'E',
-    link: function postLink(scope, element) {
-      scope.hora = new Date();
-      element.find('button').click(function (){
-        scope.$apply(function() {
-          scope.hora = new Date();
+    return {
+      templateUrl: 'templates/reloj3.html',
+      restrict: 'E',
+      link: function postLink(scope, element) {
+        scope.hora = new Date();
+        element.find('button').click(function (){
+          scope.$apply(function() {
+            scope.hora = new Date();
+          });
         });
-      });
-    }
-  };
-});
+      }
+    };
+  });
