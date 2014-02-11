0d9c1f5586179b5ef9a83710c6dd915d2fb85ec1
Gonzalo Ruiz de Villa <gonzalo.ruizdevilla@adesis.com>
2014-02-11 01:05:19 +0100

    feat(matryoshka): ejercicio 7

--- app/scripts/controllers/ej7.js
+++ app/scripts/controllers/ej7.js
@@ -1,4 +1,21 @@
 'use strict';
 
 angular.module('directivasApp')
-  .controller('Ej7Ctrl', function () {});
+  .controller('Ej7Ctrl', function ($scope) {
+    $scope.shared = {
+      colors: [
+        '#43a1cd',
+        '#639b47',
+        '#9ac147',
+        '#e1e23b',
+        '#f7941e',
+        '#ba3e2e',
+        '#9a1d34',
+        '#662a6c',
+        '#272b66',
+        '#2d559f'
+      ]
+    };
+    $scope.shared.color = $scope.shared.colors[0];
+    $scope.nivel = 5;
+  });
--- app/scripts/directives/matryoshka.js
+++ app/scripts/directives/matryoshka.js
@@ -1,12 +1,28 @@
 'use strict';
 
 angular.module('directivasApp')
-  .directive('matryoshka', function () {
+  .directive('matryoshka', function ($compile) {
     return {
-      template: '<div></div>',
+      templateUrl: 'templates/matryoshka.html',
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the matryoshka directive');
+      link: function postLink(scope, element) {
+        var colors = scope.shared.colors,
+            template = '<matryoshka/>',
+            childDoll;
+        scope.open = false;
+        scope.nivel = scope.nivel - 1;
+        if(scope.nivel > 1) {
+          childDoll = angular.element(template);
+          $compile(childDoll)(scope.$new());
+          element.find('.childdoll').append(childDoll);
+        }
+
+
+        scope.size = scope.nivel * 50;
+        scope.changeColor = function () {
+          scope.open = !scope.open;
+          scope.shared.color = colors[(colors.indexOf(scope.shared.color) + 1) % colors.length];
+        };
       }
     };
   });
